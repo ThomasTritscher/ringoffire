9962533cab4dfb6eee82ae66585cd8d27f8c71f0
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-04-14 15:42:18 +0200

    added Game class object

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
+  game!: Game;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
-this.pickCardAnimation = true;
+    this.pickCardAnimation = true;
 
   }
 
